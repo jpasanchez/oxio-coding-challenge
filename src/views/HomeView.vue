9ca0ab3b85508eea199639990afa4c301c174391
Jean-Philippe Arias Sanchez <jpgosan@gmail.com>
2024-06-23 22:56:42 -0400

    Install vuejs-smart-table Add basic table example Add data fetching before navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VTable
+        v-if="posts"
+        :data="posts">
+      <template #head>
+        <tr>
+          <th>User Id</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </template>
+      <template #body="{ rows }">
+        <tr v-for="row in rows" :key="row.id">
+          <td>{{ row.userId }}</td>
+          <td>{{ row.title }}</td>
+          <td>{{ row.body }}</td>
+        </tr>
+      </template>
+    </VTable>
   </div>
 </template>
 
@@ -13,6 +29,30 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data: () => ({
+    posts: [],
+    error: null,
+  }),
+  async beforeRouteEnter(to, from, next) {
+    try {
+      await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => next(vm => vm.setPost(json)));
+    } catch (err) {
+      next(vm => vm.setError(err));
+    }
+  },
+  mounted() {
+    // console.log('mounted');
+  },
+  methods: {
+    setPost(posts) {
+      this.posts = posts;
+    },
+    setError(err) {
+      this.error = err.toString();
+    }
   }
 }
 </script>
